0fea50c0b472b457b0f32edc76781cb04073bd8c
Efrain Lemus <efraintlemusm@gmail.com>
2022-04-24 20:05:50 -0600

    made first small changes

---
+++ components/Sidebar.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <Container>
+        <Header>
+            <h1>this is the header</h1>
+        </Header>
+    </Container>
+  )
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Header = styled.div``;
+
+const UserAvatar = styled.div``;
