2cb73d75b99e856fded216cac7f5928c08d51255
Efrain Lemus <efraintlemusm@gmail.com>
2022-05-16 00:19:24 -0400

    added icons with material ui but from version 4, version 5 was not working

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,33 @@
 // import React from 'react'
+import { Avatar, IconButton } from '@material-ui/core';
 import styled from "styled-components";
+import ChatIcon from '@material-ui/icons/Chat';
+// import ChatIcon from '@mui/icons-material/Chat';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Sidebar() {
   return (
     <Container>
-        <Header>
-            <h1>this is the header</h1>
-        </Header>
+      <Header>
+        {/* <h1>this is the header</h1> */}
+
+        <UserAvatar />
+
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+
+
+        </IconsContainer>
+
+      </Header>
     </Container>
   )
 }
@@ -15,6 +36,16 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+display:flex;
+position:sticky;
+top:0;
+background-color:white;
+z-index:1;
+justify-content:space-between;
+`;
 
-const UserAvatar = styled.div``;
+const UserAvatar = styled(Avatar)``;
+
+const IconsContainer = styled.div``;
+// const AccountCircleIcon = styled.div``;
